a0b34a2ec3ffe08e4440e83b67bb62ad7feed81d
dlschoof <dlschoof@mtu.edu>
2021-06-29 13:47:27 -0400

    Set up project base & packages

---
+++ index.js
@@ -0,0 +1,18 @@
+const cron = require("cron");
+const puppeteer = require("puppeteer");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const getPrice = async url => {
+	return;
+};
+
+const checkPrices = async () => {
+	return;
+};
+
+const sendPriceMail = async (product, price, link) => {
+	return;
+};
+
+console.log(process.env.DB_URL);
