0948b3a6789637d1d45d894ea77ee2a64fcfb4a1
dlschoof <dlschoof@mtu.edu>
2021-06-30 15:31:14 -0400

    Documented Functions

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const axios = require("axios");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
+/*
+	Gets an item's name and current price from the given url (only works on Amazon.com for now)
+	@param url - must be a string starting with amazon.com/, the url of the product that you want to track
+	@return - if the url is valid and the price and name are usable, returns an object with name and price. Otherwise, null
+*/
 const getItem = async url => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
@@ -23,6 +28,7 @@ const getItem = async url => {
 				price: price_selector.innerText,
 			};
 		});
+
 		if (
 			!item.name ||
 			!item.price ||
@@ -40,14 +46,34 @@ const getItem = async url => {
 	return null;
 };
 
+/*
+	Checks all the items in the db to get the price and email the user if the price has dropped.
+*/
 const checkPrices = async () => {
+	// Check if item is available
+	// Check if item is different price
+	// Update current price if changes, if lower email
 	return;
 };
 
+/*
+	Sends an email to the user's email with the new price and a link to the item
+	@param product - the name of the product that is being tracked
+	@param price - the new lower price of the product (in a number)
+	@param url - the url of the item being tracked
+	@param email - the email address to send the item information to
+*/
 const sendPriceMail = async (product, price, url, email) => {
+	// Send email with "Price Drop - product"
+	// Give item, price, original price (discount?), and url
 	return;
 };
 
+/*
+	Tries to get an item's info and tries to log the info to the database
+	@param email - the user's email to send a message to if the price drops
+	@param url - the url of the item to be tracked
+*/
 const logNewPrice = async (email, url) => {
 	const item = await getItem(url);
 	if (item === null) {
@@ -58,27 +84,32 @@ const logNewPrice = async (email, url) => {
 	}
 
 	try {
+		price = item.price.match(/[0-9]+.[0-9]+/g)[0];
 		const res = await axios.post(`${process.env.DB_URL}/prices.json`, {
 			email,
 			url,
 			name: item.name,
-			price: Number(item.price.match(/[0-9]+.[0-9]+/g)[0]),
+			original_price: Number(price),
+			current_price: Number(price),
 		});
+
 		if (res.status !== 200 || res.statusText !== "OK") {
 			throw new Error("Server error");
 		}
+
 		console.log(`\n[+]\t ${item.name}`);
-		console.log(`[+]\t added at price ${item.price}\n`);
-		return "OK";
+		console.log(`[+]\t added at price ${price}\n`);
+		return;
 	} catch (err) {
 		console.log(err.message);
 		console.log(
 			"\n[-]\tWe're having server issues right now.\n[-]\tPlease try again later!\n",
 		);
-		return "ERROR";
+		return;
 	}
 };
 
+// Schedule for every morning
 // cron.schedule("* * * * *", () => {
 // 	console.log("running a task every minute");
 // });
