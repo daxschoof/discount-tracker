262d71a9cfc62a0ac88e20cc2c63c2cea572b4dd
dlschoof <dlschoof@mtu.edu>
2021-07-09 23:42:25 -0400

    Started Price Check Fn
    Ran into captcha error, will continue later

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const getItem = async url => {
 
 	try {
 		await page.goto(url);
+
 		item = await page.evaluate(() => {
 			const name = document.querySelector("#productTitle");
 
@@ -42,7 +43,7 @@ const getItem = async url => {
 			throw new Error("Error!");
 		}
 	} catch (err) {
-		console.log(err.message);
+		console.error(err.message);
 	}
 
 	await browser.close();
@@ -59,7 +60,51 @@ const checkPrices = async () => {
 	// Update current price if changes, if lower email
 
 	try {
-	} catch (err) {}
+		const res = await axios.get(`${process.env.DB_URL}/prices.json`);
+		const prices = [];
+
+		for (let item_key in res.data) {
+			prices.push({
+				current_price: res.data[item_key].current_price,
+				original_price: res.data[item_key].original_price,
+				name: res.data[item_key].name,
+				email: res.data[item_key].email,
+				url: res.data[item_key].url,
+				item_key: item_key,
+			});
+		}
+
+		for (let i = 0; i < prices.length; i++) {
+			const new_item = await getItem(prices[i].url);
+			new_item.price = parseFloat(
+				new_item.price.match(/[0-9]+.[0-9]+/g)[0].replace(/,/g, ""),
+			);
+
+			if (new_item.price !== prices[i].current_price) {
+				await axios.put(
+					`${process.env.DB_URL}/prices/${prices[i].item_key}.json`,
+					{
+						current_price: new_item.price,
+						original_price: prices[i].original_price,
+						name: prices[i].name,
+						email: prices[i].email,
+						url: prices[i].url,
+					},
+				);
+				if (new_item.price < prices[i].original_price) {
+					await sendPriceMail(
+						prices[i].name,
+						price[i].original_price,
+						new_item.price,
+						prices[i].url,
+						prices[i].email,
+					);
+				}
+			}
+		}
+	} catch (err) {
+		console.error(err.message);
+	}
 
 	return;
 };
@@ -82,7 +127,7 @@ const sendPriceMail = async (product, originalPrice, newPrice, url, email) => {
 				pass: process.env.EMAIL_PASS,
 			},
 		});
-		console.log(process.env.EMAIL_PASS);
+
 		let emailText = `There has been a price drop on item: ${product}!`;
 		let htmlText = `\n\nOriginal Price: $${originalPrice}`;
 		htmlText += `\nNew Price: $${newPrice}`;
@@ -96,9 +141,10 @@ const sendPriceMail = async (product, originalPrice, newPrice, url, email) => {
 			text: `${emailText}${htmlText}`,
 			html: `<b>${emailText}</b>${htmlText}`,
 		});
+		console.log("Success! Email sent.");
 	} catch (err) {
 		console.log("Server Error! Please try again later");
-		console.log(err.message);
+		console.error(err.message);
 	}
 	return;
 };
@@ -118,13 +164,15 @@ const logNewPrice = async (email, url) => {
 	}
 
 	try {
-		price = item.price.match(/[0-9]+.[0-9]+/g)[0];
+		const price = parseFloat(
+			item.price.match(/[0-9]+.[0-9]+/g)[0].replace(/,/g, ""),
+		);
 		const res = await axios.post(`${process.env.DB_URL}/prices.json`, {
 			email,
 			url,
 			name: item.name,
-			original_price: Number(price),
-			current_price: Number(price),
+			original_price: price,
+			current_price: price,
 		});
 
 		if (res.status !== 200 || res.statusText !== "OK") {
@@ -135,7 +183,7 @@ const logNewPrice = async (email, url) => {
 		console.log(`[+]\t added at price ${price}\n`);
 		return;
 	} catch (err) {
-		console.log(err.message);
+		console.error(err.message);
 		console.log(
 			"\n[-]\tWe're having server issues right now.\n[-]\tPlease try again later!\n",
 		);
@@ -143,17 +191,13 @@ const logNewPrice = async (email, url) => {
 	}
 };
 
-// Schedule for every morning
-// cron.schedule("* * * * *", () => {
-// 	console.log("running a task every minute");
-// });
-
 const test = async () => {
 	// for (item of item_list) {
 	// 	const rand_index = Math.floor(Math.random() * email_list.length);
 	// 	await logNewPrice(email_list[rand_index], item);
 	// }
-	// sendPriceMail("Dell XPS 13", 400, 350, "amazon.com", "test@gmail.com");
+	// sendPriceMail("Dell XPS 13", 400, 350, "amazon.com", "daxloki@gmail.com");
+	await checkPrices();
 };
 
 test();
